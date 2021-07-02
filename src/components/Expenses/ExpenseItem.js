f3089f9a1a98ca9c8eab85be32dab3b065c0524d
Mark Atherton <markatherton@talkholiday.com>
2021-07-02 12:42:35 +0100

    end of lesson 49

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,6 +6,7 @@ import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
+  console.log("ExpensItem evaluated by React!");
 
   const clickHandler = () => {
     setTitle("Updated!");
