2e96a66a5d925c6afa1fbb627e4aa0c60a941bbc
Mark Atherton <markatherton@talkholiday.com>
2021-07-06 15:45:09 +0100

    end of lesson 58: finished pass data up to <App />

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,15 +2,16 @@ import React from "react";
 import ExpenseForm from "./ExpenseForm";
 
 import "./NewExpense.css";
-const saveExpenseDataHandler = (enteredExpenseData) => {
-  const expenseData = {
-    ...enteredExpenseData,
-    id: Math.random().toString(),
-  };
-  console.log(expenseData);
-};
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+  };
+
   return (
     <div className="new-expense">
       <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
