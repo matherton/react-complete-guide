c1dc70c1d20d48063311e03e46ad087771eeba31
Mark Atherton <markatherton@talkholiday.com>
2021-07-06 15:17:20 +0100

    1st half of lesson 58: Added funtion prop on <ExpenseForm /> in order to receive data in <NewExpense /> parent

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,11 +2,18 @@ import React from "react";
 import ExpenseForm from "./ExpenseForm";
 
 import "./NewExpense.css";
+const saveExpenseDataHandler = (enteredExpenseData) => {
+  const expenseData = {
+    ...enteredExpenseData,
+    id: Math.random().toString(),
+  };
+  console.log(expenseData);
+};
 
 const NewExpense = () => {
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
