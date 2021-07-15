82f208ea3dd48794f8da70403ab2d23cfa449b5b
Mark Atherton <markatherton@talkholiday.com>
2021-07-15 15:51:39 +0100

    fixed drop down dates and values

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -13,10 +13,10 @@ const ExpenseFilter = (props) => {
     <div className="expenses-filter expenses-filter__control">
       <label>Filter by year:</label>
       <select value={props.selected} onChange={yearHandler}>
+        <option value="2019">2019</option>
+        <option value="2020">2020</option>
         <option value="2021">2021</option>
         <option value="2022">2022</option>
-        <option value="2023">2023</option>
-        <option value="2024">2024</option>
       </select>
     </div>
   );
